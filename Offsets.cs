f6ef035ff31fe6342338c417fcf269943bfb38b0
bestluadev <161911209+bestluadev@users.noreply.github.com>
2024-03-01 22:32:56 -0500

    Create Offsets.cs

---
+++ Offsets.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HUNT_S_CHEAT_CS2
+{
+    internal class Offsets
+    {
+        // client
+
+
+
+        public int viewAngles = 0x19233E0;
+        public int viewMaxtrix = 0x19159A0;
+        public int localPlayer = 0x1736BC0;
+
+
+        // entity offsets
+        public int entityList = 0x1729348;
+        public int teamNum = 0x3cb;
+        public int jumpFlag = 0x3d4;
+        public int health = 0x334;
+        public int origin = 0xD60;
+    }
+}
