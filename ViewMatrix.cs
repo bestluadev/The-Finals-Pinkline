3439198c03f855e143672c1e50fdd2f331861789
bestluadev <161911209+bestluadev@users.noreply.github.com>
2024-03-01 22:33:26 -0500

    Create ViewMatrix.cs

---
+++ ViewMatrix.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HUNT_S_CHEAT_CS2
+{
+    internal class ViewMaxtrix
+    {  // repersent our 4x4 matrix a little better than just a float aray.
+        public float m11, m12, m13, m14;
+        public float m21, m22, m23, m24;
+        public float m31, m32, m33, m34;
+        public float m41, m42, m43, m44;
+    }
+}
